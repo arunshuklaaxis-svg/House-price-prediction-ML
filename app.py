1df7870e0c6732c1ffb4a9f1bae93f948b49b836
arunshuklaaxis-svg <arunshuklaaxis@gmail.com>
2025-12-28 15:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,15 @@ from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 st.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')
 
+df=pd.read_csv('house_data.csv')
+x = df.iloc[:,:-3]
+y = df.iloc[:,-1]
 
+st.sidebar.title('🏠 select house feature')
+st.sidebar.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')sidebar.
+all_value = []
+for i in x:
+  ans = st.sidebar.slider(f'select {i} value',) 
+  all_value.append(ans)
+
+st.write(all_value)
