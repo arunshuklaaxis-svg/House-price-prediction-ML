2fac10af44667b1ad16d37fbd672a0e1440480d8
arunshuklaaxis-svg <arunshuklaaxis@gmail.com>
2025-12-28 14:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠House Price prediction using ML')
+st.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')
+
 
