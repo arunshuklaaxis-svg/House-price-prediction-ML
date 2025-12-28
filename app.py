183e1846d7a48b76edc6d02b0325b9a80b064fee
arunshuklaaxis-svg <arunshuklaaxis@gmail.com>
2025-12-28 15:21:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ x = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
 st.sidebar.title('🏠 select house feature')
-st.sidebar.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')sidebar.
+st.sidebar.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')
 all_value = []
 for i in x:
   ans = st.sidebar.slider(f'select {i} value',) 
   all_value.append(ans)
 
 st.write(all_value)
+
 
 
 
