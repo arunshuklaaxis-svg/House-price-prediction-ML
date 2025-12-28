c45a4888a06dd0bb26a2f638104b897d19efd0b7
arunshuklaaxis-svg <arunshuklaaxis@gmail.com>
2025-12-28 15:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
 
 st.title('🏠House Price prediction using ML')
 st.sidebar.image('https://i.pinimg.com/originals/4b/7e/69/4b7e69a0eb1cf87c5487634c35c4c552.gif')
@@ -22,8 +23,17 @@ for i in x:
   all_value.append(ans)
 
 # st.write(all_value)
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+final_value = scaler.transform([all_value])
 
-
+model = RandomForestRegressor()
+model.fit(x,y)
+house_price = model.predict(final_value)
+with st.spinner('predicting House price'):
+  time.sleep(3)
+  st.write(house_price)
+  
 
 
 
