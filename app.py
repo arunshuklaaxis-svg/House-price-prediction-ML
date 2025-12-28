c184898695b0737fe13bfe81ac890a56823b9d55
arunshuklaaxis-svg <arunshuklaaxis@gmail.com>
2025-12-28 16:08:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,13 +30,14 @@ final_value = scaler.transform([all_value])
 
 model = RandomForestRegressor()
 model.fit(X,y)
-house_price = model.predict(final_value)
+house_price = model.predict(final_value)[0][0]
 with st.spinner('predicting House price'):
   time.sleep(3)
   msg = f'''house price is: ${house_price*100000}'''
   st.success(msg)
   st.markdown('''**Design and devlopment by; Arun shukla**''')
   
+
 
 
 
